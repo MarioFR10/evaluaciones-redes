97b1c5d2118c0f612ab3cc14796695e457e2d080
MarioFR10 <mariosmite123@gmail.com>
2022-09-16 00:55:20 -0600

    added graphics in time and frequency domain

--- TareaCorta01/autrum.py
+++ TareaCorta01/autrum.py
@@ -1,10 +1,48 @@
+# Imports
 import pyaudio
 import wave
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
 
+# Constantes
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 2  # Canal izquierdo y derecho
 RATE = 44100
+
+# Mejorar aspecto de graficos
+plt.rcParams['figure.dpi'] = 100
+plt.rcParams['figure.figsize'] = (9, 7)
+
+# Funciones
+
+
+def signal_processor(file_path):
+
+    sampFreq, sound = wavfile.read(file_path)
+    sound = sound / 2.0**15
+    length_in_s = sound.shape[0] / sampFreq
+
+    time = np.arange(sound.shape[0]) / sound.shape[0] * length_in_s
+    signal = sound[:, 0]
+
+    fft_spectrum = np.fft.rfft(signal)
+    freq = np.fft.rfftfreq(signal.size, d=1. / sampFreq)
+
+    fft_spectrum_abs = np.abs(fft_spectrum)
+
+    plt.figure(1)
+    plt.plot(freq[:3000], fft_spectrum_abs[:3000])
+    plt.xlabel("frequency, Hz")
+    plt.ylabel("Amplitude, units")
+
+    plt.figure(2)
+    plt.plot(time[6000:7000], signal[6000:7000])
+    plt.xlabel("time, s")
+    plt.ylabel("Signal, relative units")
+
+    plt.show()
 
 
 def record():
@@ -15,7 +53,7 @@ def record():
                     input=True,
                     frames_per_buffer=CHUNK)
 
-    print("Inicio grabacion")
+    print("Inicio grabacion...")
 
     frames = []
 
@@ -24,9 +62,9 @@ def record():
             data = stream.read(CHUNK)
             frames.append(data)
     except KeyboardInterrupt:
-        print("Grabacion detenida")
+        print("Grabacion detenida...")
     except Exception as e:
-        print(str(e))
+        print(e)
 
     sample_width = p.get_sample_size(FORMAT)
 
@@ -59,6 +97,11 @@ def play():
 
 
 def record_to_file(file_path):
+    
+    print('#' * 80)
+    print("Por favor hable al microfono")
+    print("Presione Ctrl+C para detener la grabacion")
+    
     wf = wave.open(file_path, 'wb')
     wf.setnchannels(CHANNELS)
     sample_width, frames = record()
@@ -66,6 +109,9 @@ def record_to_file(file_path):
     wf.setframerate(RATE)
     wf.writeframes(b''.join(frames))
     wf.close()
+    
+    print("Resultado guardado en", file_path)
+    print('#' * 80)
 
 
 def menu():
@@ -80,15 +126,17 @@ def menu():
                 print("1. Audio streaming")
                 print("2. Audio batch")
                 option1 = int(input("Favor ingresar una opcion: "))
+                file_path = input(
+                    "Favor ingresar el nombre del archivo: ")
+                
                 if option1 == 1:
-                    file_path = input("Favor ingresar el nombre del archivo a guardar: ")
                     record_to_file(file_path)
+                    signal_processor(file_path)
                 elif option1 == 2:
-                    # Leer data de arhivo wav
-                    continue
+                    signal_processor(file_path)
                 else:
                     print("La opcion elegida es invalida...")
-                    
+
             elif option == 2:
                 continue
             else:
@@ -103,16 +151,4 @@ def menu():
 
 if __name__ == '__main__':
     menu()
-    # print('#' * 80)
-    # print("Por favor hable al microfono")
-    # print("Presione Ctrl+C para detener la grabacion")
-
-    # record_to_file('output.wav')
-
-    # print("Resultado guardado en ./output.wav")
-    # print('#' * 80)
-
     # play()
-    
-    
-    # atm = ATM()
